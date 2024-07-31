af2bc933a9e949d808c8eff17fb0418cc4177677
huiziang <huisiang.tiang@queuecut.com>
2024-07-31 14:00:10 +0800

    working on home page

--- cha-project/src/App.js
+++ cha-project/src/App.js
@@ -2,21 +2,24 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Header from './components/Header';
+import NavBar from './pages/NavBar'
 // import Home from './pages/Home';
 import About from './pages/About';
 import LandingPage from './pages/LandingPage';
+import { HomePage } from './pages/HomePage'
 
 function App() {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        {/* <Route path="/" element={<Home />} /> */}
-        <Route path="/about" element={<About />} />
-        <Route path="/landingpage" element={<LandingPage />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            {/* <Header /> */}
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/landingpage" element={<LandingPage />} />
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
--- cha-project/src/index.js
+++ cha-project/src/index.js
@@ -1,20 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import NavBar from './pages/NavBar';
-import { HomePage } from './pages/HomePage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<NavBar />}>
-                <Route index element={<HomePage />} />
-            </Route>
-        </Routes>
-    </BrowserRouter>
+    <React.StrictMode>
+        <App></App>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function

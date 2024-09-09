981fd17997052187afc00a3e3fb6a019b4764bb5
huiziang <huisiang.tiang@queuecut.com>
2024-09-09 15:24:05 +0800

    Fixed sidenavbar styling and toggle when click link

--- cha-project/src/components/ConfirmPopUp.js
+++ cha-project/src/components/ConfirmPopUp.js
@@ -1,31 +1,29 @@
 import React, { useEffect, useState } from "react";
 import styles from "../styles/confirmPopUp.module.css"
 
-const ConfirmPopUp = () => {
-    const [showPopup, setShowPopup] = useState(false);
+const ConfirmPopUp = ({ togglePopup }) => {
 
     const handleConfirm = () => {
+        togglePopup()
         alert("Confirmed!");
-        setShowPopup(false); // Close popup on confirm
     };
 
-    useEffect(() => {
-        if (showPopup) {
-
-        }
-    })
+    const handleCancel = () => {
+        togglePopup()
+        alert("Cancelled!");
+    }
 
     return (
         <div>
             <div className={styles.darkenDiv} />
             <div>
-                <div id="popup" className={styles.popupBox} style={{ display: showPopup ? "block" : "block" }}>
+                <div id="popup" className={styles.popupBox}>
                     <div className={styles.popupContent}>
                         <span className={styles.head}>Confirmation Pop Up</span>
                         <p className={styles.body}>Are you sure you want to proceed?</p>
                         <div className={styles.buttonDiv}>
-                            <button className={styles.cancelBtn}>Cancel</button>
-                            <button>Confirm</button>
+                            <button className={styles.cancelBtn} onClick={handleCancel}>Cancel</button>
+                            <button className={styles.confirmBtn} onClick={handleConfirm}>Confirm</button>
                         </div>
 
                     </div>

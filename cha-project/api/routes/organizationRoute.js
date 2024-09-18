ca9689cb15738572321f090547bd5f8679e4250c
DediSepteo <dedi.22@ichat.sp.edu.sg>
2024-09-18 13:39:39 +0800

    fixed conflicts

--- cha-project/api/routes/organizationRoute.js
+++ cha-project/api/routes/organizationRoute.js
@@ -1,23 +1,33 @@
 const express = require('express');
-const Customers = require('../models/organizationModel'); // Import the model for customers
+const organization = require('../models/organizationModel'); // Import the model for organization
 
 const router = express.Router();
 
-// Get all customers
+// Get all organization
 router.get('/', (req, res) => {
-    Customers.getAll((err, results) => {
+    Organization.getAll((err, results) => {
         if (err) {
-            console.error('Error fetching customers:', err);
-            return res.status(500).send('Error fetching customers');
+            console.error('Error fetching organization:', err);
+            return res.status(500).send('Error fetching organization');
         }
-        console.log('Fetched customers data:', results); // Log the results
+        console.log('Fetched organization data:', results); // Log the results
         res.setHeader('Content-Type', 'application/json');
         res.json(results);
     });
 });
 
-router.get('/', (req, res) => {
-    
-})
+router.post('/register', (req, res) => {
+    const orgData = req.body
+    console.log(orgData)
+    organization.createOrg(orgData, (err, results) => {
+        if (err) {
+            console.error('Error creating organization:', err);
+            return res.status(500).send('Error creating organization');
+        }
+        console.log('Organization has been created'); // Log the results
+        res.setHeader('Content-Type', 'application/json');
+        return res.status(201).json({ message: 'Organization created successfully', data: results });
+    });
+});
 
 module.exports = router;

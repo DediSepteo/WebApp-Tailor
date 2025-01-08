58512826df38193add53ca3a42aaa5ea120cd301
DediSepteo <dedi.22@ichat.sp.edu.sg>
2025-01-08 11:20:33 +0800

    remove landing page for testing

--- cha-project/src/App.js
+++ cha-project/src/App.js
@@ -91,8 +91,8 @@ const AppContent = () => {
 
             <ScrollTop />
             <Routes>
-                <Route path="/" element={<NewLandingPage />} />
-                <Route path="/home" element={<Home />} />
+                {/* <Route path="/" element={<NewLandingPage />} /> */}
+                <Route path="/" element={<Home />} />
                 <Route path="/about" element={<About />} />
                 <Route path="/contact" element={<Contact />} />
                 <Route path="/login" element={<Login />} />
